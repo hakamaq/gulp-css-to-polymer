f49d93da7231cbd66f6e62b159fc61de84b64481
Hakam <hakam@Hakams-MacBook-Pro.local>
2019-08-17 08:00:21 +0300

    Added concat option

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const through = require('through2');
 const path = require('path');
 const gutil = require('gulp-util');
+const File = require('vinyl');
 
-const PLUGIN_NAME = 'gulp-css-to-string';
+const PLUGIN_NAME = 'gulp-export-html';
 
 const camelCaseModuleId = (moduleId) => {
     return moduleId.replace(/^([A-Z])|[\s-_]+(\w)/g, (match, p1, p2) => {
@@ -13,29 +14,77 @@ const camelCaseModuleId = (moduleId) => {
 
 const generateModuleName = (options, file) => `${options.prefix}${path.basename(file.path, path.extname(file.path))}${options.suffix}`;
 
+const generateTemplate = (string, moduleId) => (`export const ${camelCaseModuleId(moduleId)} =  \`${string.toString('utf8')}\`; \n `);
 
-// const generatePWAStyle = (styles, moduleId) => (`import { html } from '@polymer/lit-element';
-// export const ${camelCaseModuleId(moduleId)} = html \`
-const generatePWAStyle = (string, moduleId) => (`module.exports.${camelCaseModuleId(moduleId)} =  \`${string.toString('utf8')}\`;`);
-
-module.exports = opts => through.obj((file, enc, cb) => {
-    const fileObj = file;
-    // const fileName = path.basename(file.path);
-    const moduleId = generateModuleName(opts, file);
-    const dirname = path.dirname(file.path);
-
-    if (file.isStream()) {
-        return cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+const concatTransform = ({ options }) => {
+    if (!options.concat || typeof options.concat !== 'string') {
+        return done(new gutil.PluginError(PLUGIN_NAME, `Option 'concat' must be a non-empty string, got: ${concat}`));
     }
-    if (file.isNull()) {
-        return cb(null, file);
-    }
+    let lines = [];
+    let fileObj = {};
+    return through.obj((file, enc, done) => {
 
-    const res = generatePWAStyle(file.contents, moduleId)
+        if (file.isStream()) {
+            return done(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+        }
+        if (file.isNull()) {
+            return done(null, file);
+        }
 
-    fileObj.contents = Buffer.from(res);
-    fileObj.path = `${path.join(dirname, moduleId)}.js`;
+        const moduleId = generateModuleName(options, file);
+        const result = generateTemplate(file.contents, moduleId)
 
-    return cb(null, file);
+        if (file.isBuffer()) {
+            lines.push(result)
+        }
+        done()
+    },
+        function (done) { // flush function
+            lines.push(`export default ${fileObj}`)
+            this.push(new File({
+                path: options.concat,
+                contents: Buffer.from(lines.join('\n')),
+            }))
+            done();
+        }
+    );
+}
 
-});
+// Windows path to Unix path
+// function toUnixPath(text) {
+//     return text.replace(/\\/g, '/')
+// }
+
+// function escape(text) {
+//     return text.replace(/'/g, "\\'").replace(/\r\n|\n/g, '\\n')
+// }
+
+const separateTransform = (options) => {
+    return through.obj((file, enc, done) => {
+        if (file.isStream()) {
+            return done(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+        }
+        if (file.isNull()) {
+            return done(null, file);
+        }
+
+        const moduleId = generateModuleName(options, file);
+        const dirname = path.dirname(file.path);
+        const result = generateTemplate(file.contents, moduleId)
+
+        if (file.isBuffer()) {
+            this.push(new File({
+                path: `${path.join(dirname, moduleId)}.js`,
+                contents: Buffer.from(result),
+            }))
+        }
+
+        return done();
+    });
+}
+
+module.exports = opts => {
+    opts = Object.assign({}, opts)
+    if (opts.concat) return concatTransform(opts)
+    return separateTransform(opts);
+};
